299c2a7c62a1f1b5bc4b830f230fe371d75e114f
louistsou <louis6251@gmail.com>
2022-05-04 22:08:24 +1000

    Order update

--- IoTBay/src/java/ioTBay/Order.java
+++ IoTBay/src/java/ioTBay/Order.java
@@ -1,16 +1,26 @@
-package ioTBay;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Order.model;
 
+/**
+ *
+ * @author louistsou
+ */
 public class Order {
     private int OrderID;
     private int CustomerID;
     private int ProductID;
     private int ShippingPrice;
     private int TotalPrice;
+    private String Date;
 
-public Order(int OrderID, int CustomerID, int ProductID, int ShippingPrice, int TotalPrice) {
+public Order(int OrderID, int CustomerID, int ProductID, String Date, int ShippingPrice, int TotalPrice) {
         this.OrderID = OrderID;
         this.CustomerID = CustomerID;
         this.ProductID = ProductID;
+        this.Date = Date;
         this.ShippingPrice = ShippingPrice;
         this.TotalPrice = TotalPrice;
     }
@@ -29,6 +39,14 @@ public Order(int OrderID, int CustomerID, int ProductID, int ShippingPrice, int 
 
     public void setCustomerID(int CustomerID) {
         this.CustomerID = CustomerID;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String Date) {
+        this.Date = Date;
     }
 
     public int getProductID() {
