1183b8367ede330a600c54b91bcc7ebbe81977b5
louistsou <louis6251@gmail.com>
2022-05-08 15:37:43 +1000

    Moved order.dao to ioTBay.dao, fixed ordermanager

--- IoTBay/src/java/ioTBay/dao/OrderManager.java
+++ IoTBay/src/java/ioTBay/dao/OrderManager.java
@@ -1,24 +1,27 @@
+package ioTBay.dao;
+
+import ioTBay.Order;
 import java.sql.*;
 import java.util.ArrayList;
 
-/* 
-* DBManager is the primary DAO class to interact with the database. 
+/*
+* DBManager is the primary DAO class to interact with the database.
 * Complete the existing methods of this classes to perform CRUD operations with the db.
 */
 
 public class OrderManager {
 
 private Statement st;
-   
-public OrderManager(Connection conn) throws SQLException {       
+
+public OrderManager(Connection conn) throws SQLException {
    st = conn.createStatement();   
 }
 
-//Find user by email and password in the database   
-public Order findOrder(int OrderID, int ProductID, int CustomerID, String Date, int ShippingPrice, int TotalPrice) throws SQLException {       
+//Find user by email and password in the database
+public Order findOrder(int OrderID, int ProductID, int CustomerID, String Date, int ShippingPrice, int TotalPrice) throws SQLException {
    String fetch = "select * from STAFF.Order where OrderID = '" + OrderID + "' and ProductID='" + ProductID +"' and CustomerID='" + CustomerID+"'";
    ResultSet rs = st.executeQuery(fetch);
-              
+
    while(rs.next()){
       int orderID = rs.getInt(1);
       String date  = rs.getString(4);
@@ -30,23 +33,23 @@ public Order findOrder(int OrderID, int ProductID, int CustomerID, String Date, 
          return new Order(orderID, productID, customerID, date, shippingPrice, totalPrice);
     }
 }
-   return null;   
+   return null;
 }
 
-//Add a user-data into the database   
-public void addOrder(int OrderID, int ProductID, int CustomerID, String Date, int ShippingPrice, int TotalPrice) throws SQLException {                   //code for add-operation       
-  st.executeUpdate("INSERT INTO STAFF.CUSTOMERORDER " + "VALUES ('" + OrderID + "', '" + ProductID + "', '"+CustomerID + "','"+ "', '"+Date + "','" + ShippingPrice + "', '" + TotalPrice +"')");   
+//Add a user-data into the database
+public void addOrder(int OrderID, int ProductID, int CustomerID, String Date, int ShippingPrice, int TotalPrice) throws SQLException {                   //code for add-operation
+  st.executeUpdate("INSERT INTO STAFF.CUSTOMERORDER " + "VALUES ('" + OrderID + "', '" + ProductID + "', '"+CustomerID + "','"+ "', '"+Date + "','" + ShippingPrice + "', '" + TotalPrice +"')");
 
 }
 
-//update a user details in the database   
-public void updateOrder(int OrderID, int ProductID, int CustomerID, String Date, int ShippingPrice, int TotalPrice) throws SQLException {       
+//update a user details in the database
+public void updateOrder(int OrderID, int ProductID, int CustomerID, String Date, int ShippingPrice, int TotalPrice) throws SQLException {
    st.executeUpdate("UPDATE STAFF.CUSTOMERORDER " + "VALUES ('" + OrderID + "', '" + ProductID + "', '"+CustomerID + "','"+ "', '"+ Date + "','" + ShippingPrice + "', '" + TotalPrice +"')");
 
-}       
+}
 
-//delete a user from the database   
-public void deleteOrder(int OrderID) throws SQLException{       
+//delete a user from the database
+public void deleteOrder(int OrderID) throws SQLException{
    st.executeUpdate("DELETE FROM STAFF>CUSTOMERORDER WEHRE OrderID='" + OrderID + "'");
 
 }
@@ -55,7 +58,7 @@ public ArrayList<Order> fectOrders() throws SQLException {
    String fetch  ="select * from STUDENTS";
    ResultSet rs = st.executeQuery(fetch);
    ArrayList<Order> temp = new ArrayList();
-   
+
    while (rs.next()){
       int orderID = rs.getInt(1);
       int customerID = rs.getInt(2);
@@ -68,6 +71,6 @@ public ArrayList<Order> fectOrders() throws SQLException {
 return temp;
 }
 
- 
+
 
 }
