95dd7163f53f5783121065f68feb7cebfaaeef19
Jaydn Chiert <jaydn.chiert@hotmail.com>
2022-05-11 14:10:15 +1000

    Fixed web.xml and ConnServlet

--- IoTBay/src/java/ioTBay/controller/ConnServlet.java
+++ IoTBay/src/java/ioTBay/controller/ConnServlet.java
@@ -9,19 +9,20 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import ioTBay.dao.*;
+
 
  
 
 public class ConnServlet extends HttpServlet {
 
     private DBConnector db;
-    private DBManager manager;
+    private UserManager manager;
     private Connection conn;
 
     @Override //Create and instance of DBConnector for the deployment session
@@ -40,7 +41,7 @@ public class ConnServlet extends HttpServlet {
         HttpSession session = request.getSession();
         conn = db.openConnection();       
         try {
-            manager = new DBManager(conn);
+            manager = new UserManager(conn);
         } catch (SQLException ex) {
             Logger.getLogger(ConnServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
