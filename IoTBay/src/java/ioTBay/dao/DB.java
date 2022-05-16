aaab2c035f7885696c59163e5a80d40c7b093f12
Jaydn Chiert <jaydn.chiert@hotmail.com>
2022-05-16 17:45:30 +1000

    Create addUser test, fix user.java db.java UserManager.java, add RegisterServlet

--- IoTBay/src/java/ioTBay/dao/DB.java
+++ IoTBay/src/java/ioTBay/dao/DB.java
@@ -10,10 +10,10 @@ import java.sql.Connection;
  * @author Jaydn
  */
 public abstract class DB {
-    protected String URL = "jdbc:derby://localhost:1527/iotdb";
+    protected String URL = "jdbc:derby://localhost:1527/";
     protected String db = "iotdb";
-    protected String dbuser = "";
-    protected String dbpass = "";
+    protected String dbuser = null;
+    protected String dbpass = null;
     protected String driver = "org.apache.derby.jdbc.ClientDriver";
     protected Connection conn;
 }
