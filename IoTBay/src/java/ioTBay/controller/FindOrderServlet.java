74639c9d9b8d4beeea0820510cb86a9052f611db
louistsou <louis6251@gmail.com>
2022-05-19 15:20:26 +1000

    Update FindOrderServlet.java

--- IoTBay/src/java/ioTBay/controller/FindOrderServlet.java
+++ IoTBay/src/java/ioTBay/controller/FindOrderServlet.java
@@ -12,7 +12,7 @@
  import javax.servlet.http.HttpServletRequest;
  import javax.servlet.http.HttpServletResponse;
  import javax.servlet.http.HttpSession;
- import ioTBay.Order.Order;
+ import ioTBay.Order;
  import ioTBay.OrderDAO.OrderManager;
 
 
@@ -44,4 +44,3 @@
              }
          }
      }
- 
