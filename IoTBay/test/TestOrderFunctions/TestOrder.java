e415461f5f5b928093c676b3d02b5da637bfef6b
louistsou <louis6251@gmail.com>
2022-05-21 15:13:15 +1000

    Update TestOrder.java

--- IoTBay/test/TestOrderFunctions/TestOrder.java
+++ IoTBay/test/TestOrderFunctions/TestOrder.java
@@ -12,34 +12,28 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author louistsou
  */
 public class TestOrder {
-
-    public TestOrder() {
+    @Test
+    public void TestOrder() {
       private static Scanner in = new Scanner(System.in);
-      public static void main(String[] args){
-          try{
+      try{
               DBConnector connector = new DBConnector();
               Connection conn = connector.openConnection();
               OrderManager db = new OrderManager(conn);
-
               System.out.print("OrderID: ");
               int orderID = in.nextInt;
-
               System.out.print("ProductID: ");
               int productID = in.nextInt;
-
               System.out.print("UserID: ");
               int userID = in.nextInt;
-
               int shippingPrice = 15;
-
               int totalPrice = 50;
-
               System.out.print("Date: ");
               String date = in.nextLine;
 
               db.addOrder(orderID, productID, userID, date, shippingPrice, totalPrice)
               Order order = new Order(orderID, productID, userID, date, shippingPrice, totalPrice);
               Order testOrder = db.findOrder(orderID, date);
+
               assertSame(order, testOrder);
               assertEquals(order.OrderID,testOrder.OrderID);
               assertEquals(order.ProductID,testOrder.ProductID);
@@ -52,6 +46,6 @@ public class TestOrder {
               Logger.getLogger(TestFindOrder.class.getName()).log(Level.SEVERE, null, ex);
           }
       }
-    }
+
 
 }
