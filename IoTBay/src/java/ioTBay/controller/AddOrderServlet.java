2497959a053f3b7ab060699a501fff630d31bb20
louistsou <louis6251@gmail.com>
2022-05-22 14:25:45 +1000

    Update AddOrderServlet.java

--- IoTBay/src/java/ioTBay/controller/AddOrderServlet.java
+++ IoTBay/src/java/ioTBay/controller/AddOrderServlet.java
@@ -13,7 +13,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpSession;
 import ioTBay.Order;
 import ioTBay.OrderDAO.OrderManager;
 /**
